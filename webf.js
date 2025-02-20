ae1819d96a673f9087484746cb98587dfd0007d4
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-20 15:24:40 +0900

    Create webf.js

---
+++ webf.js
@@ -0,0 +1,5 @@
+import {WebVariable, GETWORK, DIV, searchParams, WFDI, compile, WebF, deferapp, callWebc, callWeber, getter, f} import "https://FarAway6834.github.io/webf/lib.js";
+deferapp()
+
+//use it by <script defer src = "https://FarAway6834.github.io/webf/webf.js" />
+//unstable way : <script defer src = "http://webf.kro.kr" />
