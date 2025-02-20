a377c72e9d700b57865c214848a12fb13d4b6874
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-20 15:27:36 +0900

    Update webf.js

--- webf.js
+++ webf.js
@@ -2,4 +2,4 @@ import {WebVariable, GETWORK, DIV, searchParams, WFDI, compile, WebF, deferapp, 
 deferapp()
 
 //use it by <script defer src = "https://FarAway6834.github.io/webf/webf.js" />
-//unstable way : <script defer src = "http://webf.kro.kr" />
+//unstable way : <script defer src = "http://wbf.kro.kr" />
